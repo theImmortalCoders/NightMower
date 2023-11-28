eb4afcc0fb2528bb47e5ecc97ef4315576c2b0d4
marcinbator <marcinbator.ofc@gmail.com>
2023-11-28 08:51:07 +0100

    textures working

--- szescian/loader.cpp
+++ szescian/loader.cpp
@@ -34,23 +34,41 @@ void loadTexture() {
 void loadObjFile(const std::string& filename, GLfloat red, GLfloat green, GLfloat blue, int x, int y, int z, int scaleX, int scaleY, int scaleZ)
 {
     std::vector<float> vertices;
+    std::vector<float> textures; // Added texture coordinates
     tinyobj::attrib_t attrib;
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> materials;
     std::string err;
     bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, filename.c_str());
+
     for (const auto& shape : shapes) {
         for (const auto& index : shape.mesh.indices) {
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 0]);
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 1]);
             vertices.push_back(attrib.vertices[3 * index.vertex_index + 2]);
+
+            if (!attrib.texcoords.empty()) {
+                textures.push_back(attrib.texcoords[2 * index.texcoord_index + 0]);
+                textures.push_back(attrib.texcoords[2 * index.texcoord_index + 1]);
+            }
         }
     }
+
+    // Rendering with texture coordinates
+    glEnable(GL_TEXTURE_2D);
+    loadTexture();
+
     glBegin(GL_TRIANGLES);
     glColor3f(red, green, blue);
     for (size_t i = 0; i < vertices.size(); i += 3) {
-        glVertex3f(scaleX * vertices[i] + x, scaleY * (vertices[i + 1]) + y, scaleZ * vertices[i + 2] + z);
+        // Ensure there are enough texture coordinates for each vertex
+        if (i + 2 < textures.size()) {
+            glTexCoord2f(textures[i], textures[i + 1]);  // Use texture coordinates
+        }
+
+        glVertex3f(scaleX * vertices[i] + x, scaleY * vertices[i + 1] + y, scaleZ * vertices[i + 2] + z);
     }
     glEnd();
-    loadTexture();
+
+    glDisable(GL_TEXTURE_2D);
 }
